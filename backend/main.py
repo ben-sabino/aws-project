94897241fe129a4d2f0af5f2f47bdb20bc086c09
GuilberthBruno <guilberthbruno1@gmail.com>
2025-06-10 20:58:58 -0300

    adicionando instancia como origem de chamada valida

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,8 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["http://localhost:5173",
+                   "http://107.20.88.199:5173"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
